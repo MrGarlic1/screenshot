0e0b60bf21a6614a3e7bc779f4bb6602eca6c4fc
MrGarlic1 <ben@samans.com>
2025-04-04 12:19:20 -0400

    feat: screen capture

---
+++ main.py
@@ -0,0 +1,31 @@
+from pyautogui import size as monitor_size
+from PIL import ImageGrab
+from datetime import datetime
+from pathlib import Path
+from time import sleep
+
+
+def capture_screenshot() -> ImageGrab:
+    width, height = monitor_size()
+    image = ImageGrab.grab(bbox=(0, 0, width, height))
+    max_width = 1920
+    scale_factor = max_width / width
+    image = image.resize(size=(round(width * scale_factor), round(height * scale_factor)))
+    return image
+
+
+def main():
+    subdir = input("Enter Folder Name: ")
+    folder_path = f"Screenshots/{subdir}"
+    Path(folder_path).mkdir(parents=True, exist_ok=True)
+    print(f"Starting screen captures. Saving to {folder_path}.")
+
+    screenshots = 0
+    while screenshots < 1000:
+        image = capture_screenshot()
+        image.save(f"{folder_path}/{datetime.now().strftime("%Y%m%d%H%M%S")}.png")
+        sleep(15)
+
+
+if __name__ == "__main__":
+    main()
