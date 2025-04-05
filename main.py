5d0b99ff3e4f1ae9edacf7d71c481a42901b2be3
MrGarlic1 <ben@samans.com>
2025-04-04 20:41:03 -0400

    fix: aspect ratio wizardry

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ from time import sleep
 
 def capture_screenshot() -> ImageGrab:
     width, height = monitor_size()
-    image = ImageGrab.grab(bbox=(0, 0, width, height))
+    target_ratio = 16/9
+
+    if width/height < target_ratio:  # Screen too tall (e.g 16:10, 4:3)
+        target_height = width * 9 / 16
+        target_width = width
+    else:  # Screen too wide (e.g. 21:9, ultrawide)
+        target_width = height * 16 / 9
+        target_height = height
+
+    image = ImageGrab.grab(
+        bbox=((width - target_width)/2, (height - target_height)/2, width - (width - target_width)/2, height - (height - target_height)/2)
+    )
     max_width = 1920
     scale_factor = max_width / width
     image = image.resize(size=(round(width * scale_factor), round(height * scale_factor)))
